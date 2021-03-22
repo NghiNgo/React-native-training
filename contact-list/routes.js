793ece0591871349ef4091ff4c5cb4790ca104f3
Nghi Ngo <74362909+NghiNgo@users.noreply.github.com>
2021-03-22 16:24:17 +0700

    TopTab Navigation

--- contact-list/routes.js
+++ contact-list/routes.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
 import User from './screens/User';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+const Tab = createMaterialTopTabNavigator();
 
-export default function StackNavigator() {
+export default function TabNavigator() {
   return (
-    <Stack.Navigator initialRouteName='User'>
-      <Stack.Screen
+    <Tab.Navigator>
+      <Tab.Screen
         name="Contacts"
         component={Contacts}
       />
-      <Stack.Screen
+      <Tab.Screen
         name="Profile"
         component={Profile}
       />
-      <Stack.Screen
+      <Tab.Screen
         name="User"
         component={User}
       />
-    </Stack.Navigator>
+    </Tab.Navigator>
   );
 }
